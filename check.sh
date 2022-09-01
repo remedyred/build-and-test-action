7f5ed8d8fa3a4258adec16ad3ff0d586536ea362
Snickbit <snickbit@users.noreply.github.com>
2022-08-31 22:50:17 -0400

    fix: script checking

--- check.sh
+++ check.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
+AVAILABLE_SCRIPTS=$(pnpm run)
+
 has_script() {
   local script_name="$1"
   local SCRIPT_COUNT
-  SCRIPT_COUNT=$(pnpm run | grep -c "^  $script_name")
+  SCRIPT_COUNT=$(echo "$AVAILABLE_SCRIPTS" | grep -c "^  $script_name")
   [[ $SCRIPT_COUNT -gt 0 ]]
 }
+
 
 echo "Install dependencies"
 pnpm install --frozen-lockfile
 
 
-if [[ $(has_script "build") ]]; then
+if has_script "build"; then
   echo "Running build"
   pnpm run build
 else
   echo "No build script"
 fi
 
-if [[ $(has_script "test") ]]; then
+if has_script "test"; then
   echo "Running tests"
   pnpm run test
 else
   echo "No test script"
 fi
 
-if [[ $(has_script "lint") ]]; then
+if has_script "lint"; then
   echo "Checking lint"
   pnpm run lint
 else
   echo "No lint script"
 fi
 
-if [[ $(has_script "docs") ]]; then
+if has_script "docs"; then
   echo "Checking docs"
   git reset --hard > /dev/null 2>&1
   pnpm run docs
